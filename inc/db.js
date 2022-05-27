01cd67b86a6b06c4f3adf568f0535ce79653177e
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-05-27 10:18:36 -0300

    inicialize project

---
+++ inc/db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2')
+
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'userP', //meu usuario criado no mysql para nao confundi 
+    database: 'saborosoPedro',
+    password: 'password'
+});
+
+module.exports = connection;
