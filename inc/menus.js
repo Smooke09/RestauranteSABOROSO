ba9db09e4ed77eb2e0e5f96574446232d1970386
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-05-30 08:09:24 -0300

    Refactoring HEADER and FOOTER

--- inc/menus.js
+++ inc/menus.js
@@ -1,13 +1,9 @@
 let conn = require('./db');
 
-
 module.exports = {
 
     getMenus() {
-
-
         return new Promise((resolve, reject) => {
-
             conn.query(`
             SELECT * FROM tb_menus ORDER BY title
           `, (err, results) => {
