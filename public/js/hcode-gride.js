115909a4a3507fc02cecebc89fad9c5f74f64a81
Pedro Lucas <pedrolucas1014zz@gmail.com>
2022-06-02 09:49:04 -0300

    events with componets

--- public/js/hcode-gride.js
+++ public/js/hcode-gride.js
@@ -2,6 +2,12 @@ class HcodeGrid {
 
     constructor(configs) {
 
+        configs.listeners = Object.assign({
+            afterUpdateClick: (e) => {
+                $('#modal-update').modal('show')
+            }
+        }, configs.listeners)
+
         this.options = Object.assign({}, {
             formCreate: '#modal-create form',
             formUpdate: '#modal-update form',
@@ -40,11 +46,19 @@ class HcodeGrid {
 
     }
 
+    fireEvent(name, args) {
+
+        if (typeof this.options.listeners[name] === 'function') this.options.listeners[name].apply(this, args)
+    }
+
 
     initButtons() {
 
         [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
+
             btn.addEventListener('click', e => {
+
+                this.fireEvent('beforeUpdateClick', [e]);
 
                 let tr = e.path.find(el => {
 
@@ -67,7 +81,9 @@ class HcodeGrid {
 
                     }
                 }
-                $('#modal-update').modal('show')
+                this.fireEvent('afterUpdateClick', [e]);
+
+
             });
 
         });
